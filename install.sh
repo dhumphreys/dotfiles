27298056d346db7bf980df362c0aa6e18084333e
Don Humphreys <dhumphreys88@gmail.com>
2016-12-29 01:09:19 -0500

    Added more required programs and symlinks to install script

--- install.sh
+++ install.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
+
+# find current dotfile directory
+DOTFILES="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# install required programs
 sudo apt-get install \
   compton \
+  dropbox \
   feh \
+  fonts-font-awesome \
   fonts-hack-ttf \
   i3 \
   i3blocks \
   powerline \
+  vim \
+  wicd \
   xbacklight
+
+# link config directories to dotfiles checkout
+ln -s $DOTFLIES/i3 $HOME/.config/i3
+ln -s $DOTFLIES/i3blocks $HOME/.config/i3blocks
+
+# link common folders to Dropbox
+ln -s $HOME/Dropbox/Documents $HOME/Documents
+ln -s $HOME/Dropbox/Photos $HOME/Pictures
+ln -s $HOME/Dropbox/Music $HOME/Music
+ln -s $HOME/Dropbox/Videos $HOME/Videos
